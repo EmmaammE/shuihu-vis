f29dc7be79af362510803fdb04e12f172182629e
zhuzhu <zrcccrz@163.com>
2019-05-29 22:11:04 +0800

    完善zoom

--- src/component/lineChart/lineChart.js
+++ src/component/lineChart/lineChart.js
@@ -2,6 +2,7 @@ import { lineGraphSetting } from "../../util/setting";
 import "./lineChart.css"
 
 const _width = window.innerWidth - lineGraphSetting.left - lineGraphSetting.right;
+//event_level:1，先显示； 2，后显示；如果undefined->3
 export default class LineChart {
     constructor(opts) {
         this.data = opts.data;
@@ -18,10 +19,11 @@ export default class LineChart {
 
     draw() {
         this.createScales();
-        this.addAxes();
         this.createLines();
+
+        this.addAxes();
         this.addZoomListener();
-        this.addZoomButtons();
+        // this.addZoomButtons();
     }
 
     addZoomButtons() {
@@ -36,21 +38,38 @@ export default class LineChart {
     }
 
     addZoomListener() {
-
         let that = this;
         function zoomed() {
             let t = d3.event.transform;
-            if ( t.k <= 1 ) {
-                that.updateLines(that.xScale,d=>that.xScale(d['编号']))
-                return;
-            }
-            console.log(t);
+            // if ( t.k <= 1 ) {
+            //     that.xAxis.ticks(60);
+            //     that.updateLines(that.xScale,d=>that.xScale(d['编号']))
+            //     return;
+            // }
+            // console.log(t);
+
+            // let xNewScale = t.rescaleX(that.xScale);
+            // console.log(xNewScale.domain());
+            // let toX = d => xNewScale(d['编号']);
+            // that.xAxis.ticks(120/t.k);
+            // that.updateLines(xNewScale, toX);
 
             let xNewScale = t.rescaleX(that.xScale);
-            console.log(xNewScale.domain());
+            console.log(t.k);
             let toX = d => xNewScale(d['编号']);
-            that.updateLines(xNewScale, toX);
 
+            if (t.k <= 1) {
+                that.xAxis.ticks(60);
+                that.updateLines(that.xScale, d => that.xScale(d['编号']), 1)
+                console.log('debug:', 1);
+            } else if ( 2.2<=t.k && t.k<5.6) {
+                that.xAxis.ticks(120 / t.k);
+                that.updateLines(xNewScale, toX, 2);
+                console.log('debug:', 2);
+            } else if (t.k >=6) {
+                that.xAxis.ticks(120 / t.k);
+                that.updateLines(xNewScale, toX, 3);
+            }
         }
         var zoom = d3.zoom()
         .scaleExtent([1,10])
@@ -64,12 +83,11 @@ export default class LineChart {
             .call(zoom);
     }
 
-    updateLines(_scale,_scaleToX) {
+    updateLines(_scale,_scaleToX,level) {
         let self = this;
         d3.select('g.x').transition().call(self.xAxis.scale(_scale));
         d3.selectAll('.line')
             .transition()
-            // .style("stroke-width", 2 * (t / 10))
             .attr('d', d3.line()
                 .x(_scaleToX)
                 .y(function (d) {
@@ -77,17 +95,55 @@ export default class LineChart {
                 }));
 
         let legends = d3.select('g.legends')
+
             .selectAll('g');
+
+
         legends.select('line')
             .transition()
             .attr('x1', _scaleToX)
-            .attr('x2', _scaleToX);
+            .attr('x2', _scaleToX)
+            .attr("stroke-dasharray", "2,2")
+
+            .attr('class', d => {
+                switch (level) {
+                    case 3:
+                        if (!d.level || d.level <= level) {
+                            return 'legend-line';
+                        } else {
+                            return '';
+                        }
+                    default:
+                        if (d.level && d.level <= level) {
+                            return 'legend-line';
+                        } else {
+                            return '';
+                        }
+                }
+            })
         legends.select('circle')
             .transition()
             .attr('transform', d => 'translate(' + _scaleToX(d) + ',' + self.yScale(d.personArr.length) + ')');
         legends.select('text')
             .transition()
-            .attr('x', _scaleToX);
+            .attr('x', _scaleToX)
+            .text(d => {
+                switch (level) {
+                    case 3:
+                        if (d.level <= level || d.level == undefined) {
+                            return d['abbr'];
+                        } else {
+                            return '';
+                        }
+                    default:
+                        if (d.level && d.level <= level) {
+                            return d['abbr'];
+                        } else {
+                            return '';
+                        }
+                }
+            })
+            .style('font-size', `${9 + level}px`);
     }
     createScales() {
         this.xScale = d3.scaleLinear().range([0,_width - lineGraphSetting.right - lineGraphSetting.left]).domain([0,this.data.length]);
@@ -135,6 +191,8 @@ export default class LineChart {
             .data(this.data)
             .enter()
             .append('g')
+
+
         let _random = function (seed) {
             return ('0.' + Math.sin(seed).toString().substr(6));
         }
@@ -142,15 +200,22 @@ export default class LineChart {
         let _x1 = (d) => this.xScale(d['编号'])
         let _y1 = (d) => this.yScale(d.personArr.length);
         let _x2 = (d) => this.xScale(d['编号']);
-        let _y2 = (d) => (lineGraphSetting.lineGraphHeight - this.yScale(d.personArr.length) + 100 * _random(d['编号']))
+        // let _y2 = (d) => (80 * _random(d['编号']) - lineGraphSetting.lineGraphHeight + this.yScale(d.personArr.length) )
+        // let _y2 = (d) =>- lineGraphSetting.bottom +  this.yScale(d.personArr.length)
+        let _y2 = (d) => {
+            if (d.personArr.length > 100) {
+                return lineGraphSetting.bottom;
+            }
+            return lineGraphSetting.bottom - lineGraphSetting.lineGraphHeight / 1.1 + this.yScale(d.personArr.length);
+        }
 
         legends.append('line')
-            // .filter(d, d['abbr']=='')
-            .attr("stroke-dasharray", "2,2")
             .attr('x1',d => _x1(d))
             .attr('y1',d => _y1(d))
             .attr('x2',d => _x2(d))
             .attr('y2',d => _y2(d))
+            .filter(d => d.level == 1)
+            .attr("stroke-dasharray", "2,2")
             .attr('class','legend-line')
 
         legends.append('circle')
@@ -164,9 +229,24 @@ export default class LineChart {
         //     .attr('transform', d => 'translate(' + _x2(d) +',' + _y2(d) + ')')
 
         legends.append('text')
-            .text(d => d['abbr'])
             .attr('x',d => _x2(d))
             .attr('y', d => `${_y2(d) - 15}px`)
+            .style('font-size','10px')
             .attr('class','e')
+            .filter(d => d.level == 1)
+            .text(d => d['abbr'])
+
+            // 最后一个g中
+        legends.append('rect')
+            .attr('class','overlay')
+            .attr('width',lineGraphSetting.left)
+            .attr('height',lineGraphSetting.lineGraphHeight)
+            .attr('transform','translate(-'+lineGraphSetting.left+',0)')
+
+        legends.append('rect')
+            .attr('class', 'overlay')
+            .attr('width', lineGraphSetting.right)
+            .attr('height', lineGraphSetting.lineGraphHeight)
+            .attr('transform', 'translate(' + (_width - lineGraphSetting.right - lineGraphSetting.left + 5) + ',0)')
     }
 }
